b44178adbef64bd87deac19cf39859e70088a517
Bartlomiej Nowak <bartlomiej.nowak22@gmail.com>
2020-11-25 22:18:12 +0100

    Add DesktopUI WPF Project.
    Adds WPF projects and sets up Caliburn.micro

---
+++ CinemaUI/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using CinemaUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace CinemaUI
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ CinemaUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemaUI.ViewModels
+{
+    public class ShellViewModel
+    {
+    }
+}
