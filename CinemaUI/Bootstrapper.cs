9665ba51e2c1f65591310e02cad86488c89b48bd
Bartlomiej Nowak <bartlomiej.nowak22@gmail.com>
2020-11-25 22:25:53 +0100

    Further Set Up of DesktopUI
    Sets up simple container for DesktopUI and Caliburn.micro support for PasswordBox

--- CinemaUI/Bootstrapper.cs
+++ CinemaUI/Bootstrapper.cs
@@ -1,10 +1,12 @@
 ﻿using Caliburn.Micro;
+using CinemaDesktopUI.Helpers;
 using CinemaUI.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace CinemaUI
 {
@@ -13,11 +15,48 @@ namespace CinemaUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
+        }
+
+        private SimpleContainer _container = new SimpleContainer();
+
+        protected override void Configure()
+        {
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
