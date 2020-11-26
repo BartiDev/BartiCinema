919b0d4bcbe05e4f35a98e9720b964c193493240
Bartlomiej Nowak <bartlomiej.nowak22@gmail.com>
2020-11-26 01:37:26 +0100

    Add Login Form
    Adds login form without support for log in button.

--- CinemaUI/ViewModels/ShellViewModel.cs
+++ CinemaUI/ViewModels/ShellViewModel.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using CinemaDesktopUI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CinemaUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        public ShellViewModel()
+        {
+            ActivateItemAsync(IoC.Get<LoginViewModel>());
+        }
     }
 }
